32e3b926c80ec998a8b2f3a8adbc2a841631677a
haileyxc <haileychi@outlook.com>
2019-07-24 16:00:22 +0100

    wip

--- display_ad_manager/tests.py
+++ display_ad_manager/tests.py
@@ -1,6 +1,7 @@
 import unittest
 from django.test import TestCase
 from django.test import Client
+from .utils import converter
 # Create your tests here.
 
 class SimpleTest(unittest.TestCase):
@@ -9,7 +10,24 @@ class SimpleTest(unittest.TestCase):
         response = client.get('/placements/')
         self.assertEqual(response.status_code, 200)
 
-        
+
 class BasicTest(unittest.TestCase):
     def test_one_plus_one(self):
         self.assertEqual(1+1, 2)
+
+
+class ConverterTest (unittest.TestCase):
+    def test_new_line(self):
+        input= 'hsjkjh'
+        output= converter(input)
+        self.assertEqual(output, 'document.write(%s)' %input)
+
+    def test_add_line(self):
+        line1 = 'bjkds'
+        line2 = 'saklk'
+
+        input= """bjkds
+saklk"""
+        output= converter(input)
+        self.assertEqual(output, """document.write(bjkds)
+        document.write(saklk)""")
--- display_ad_manager/utils.py
+++ display_ad_manager/utils.py
@@ -8,6 +8,6 @@ def converter(lines):
     x = lines.split()
     lines2 = []
     for i in x:
-        lines2.append("\"document.write(%s)\"" % i)
-        
-    return lines2
+        lines2.append("document.write(" + i + ")")
+
+    return " ".join(lines2)
