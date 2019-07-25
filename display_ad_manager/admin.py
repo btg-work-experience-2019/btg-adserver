3e870784a720af2153275899336702720da5a704
KacperBuksa <buksa.kacper@gmail.com>
2019-07-25 14:35:48 +0100

    this is the code we added

--- display_ad_manager/admin.py
+++ display_ad_manager/admin.py
@@ -1,6 +1,20 @@
 from django.contrib import admin
-
 from .models import Placement, Banner
+
+from django import forms
 
 admin.site.register(Placement)
 admin.site.register(Banner)
+
+
+class PlacementForm(forms.ModelForm):
+    list_display = ('name')
+
+    class Meta:
+        model = Placement
+        exclude = ['name']
+
+class PlacementAdmin(admin.ModelAdmin):
+    list_display = ('name')
+    exclude = ['name']
+    form = PlacementForm
