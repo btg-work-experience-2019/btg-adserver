1848dace7a714a7d52075e44bef31f7b25c82950
slimkrazy <sam@slimkrazy.com>
2019-07-25 22:05:07 +0100

    Update placement url conf to pass placement ID

--- display_ad_manager/views.py
+++ display_ad_manager/views.py
@@ -3,7 +3,5 @@ from django.shortcuts import render
 # Create your views here.
 from django.http import HttpResponse
 
-def index(request):
-    return HttpResponse("")
-def placements(request):
-    return HttpResponse("")
+def placement(request, placement_id):
+    return HttpResponse(placement_id)
