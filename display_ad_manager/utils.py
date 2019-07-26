81f0cd283205818f7600c7f5c979e66942bcafd6
slimkrazy <sam@slimkrazy.com>
2019-07-26 12:16:57 +0100

    Fix HTML to JS function

--- display_ad_manager/utils.py
+++ display_ad_manager/utils.py
@@ -5,6 +5,6 @@ def converter(lines):
     x = lines.split("\n")
     lines2 = []
     for i in x:
-        lines2.append("document.write('" + i + "');")
+        lines2.append('document.write("' + i.strip().replace('"','\\"') + '");')
 
     return "\n".join(lines2)
